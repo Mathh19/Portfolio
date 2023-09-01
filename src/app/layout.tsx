b7694fe4105be92ed8d1fccf817d69f50c9e26d2
Matheus <mf635097@gmail.com>
2023-09-01 12:01:34 -0300

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import './globals.css';
+import { Footer } from '@components/Footer';
 import { Header } from '@components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -24,6 +25,7 @@ export default function RootLayout({
         <div className="min-h-screen flex justify-center">
           <main className="w-full max-w-[1440px]">{children}</main>
         </div>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import { MdOutlineEmail } from 'react-icons/md';
+
+export const Footer = () => {
+  return (
+    <footer className="w-full flex items-center justify-center gap-6 text-3xl px-10 py-4 border-t border-gray-color">
+      <Link href="https://github.com/Mathh19" target="_blank">
+        <BsGithub />
+      </Link>
+      <Link href="mailto:eumathfreitas@gmail.com" target="_blank">
+        <MdOutlineEmail />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/matheus-freitas-629557232/"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </Link>
+    </footer>
+  );
+};
