9a98a308eccc9dd93f6bf6f733cde1e7db4d11f6
Matheus <mf635097@gmail.com>
2023-09-04 16:27:46 -0300

    feat: add scroll to top

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { BsArrowUp } from 'react-icons/bs';
+
+export const ScrollToTop = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <a
+      href="#"
+      className={`${
+        visible ? 'visible' : 'invisible'
+      } fixed right-4 bottom-4 p-2 border bg-background-color rounded-full text-2xl duration-200 ease-in-out hover:bg-white hover:text-background-color hover:border-background-color`}
+    >
+      <BsArrowUp />
+    </a>
+  );
+};
