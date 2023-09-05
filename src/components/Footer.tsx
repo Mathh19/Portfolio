8f5a68831d7ee46e5aaf7f2fc649c1fe283f29ab
Matheus <mf635097@gmail.com>
2023-09-04 21:22:27 -0300

    fix: add description for each link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,22 @@ import { MdOutlineEmail } from 'react-icons/md';
 export const Footer = () => {
   return (
     <footer className="w-full flex items-center justify-center gap-6 text-3xl px-10 py-4 border-t border-gray-color">
-      <Link href="https://github.com/Mathh19" target="_blank">
+      <Link
+        aria-label="link para o perfil do github"
+        href="https://github.com/Mathh19"
+        target="_blank"
+      >
         <BsGithub />
       </Link>
-      <Link href="mailto:eumathfreitas@gmail.com" target="_blank">
+      <Link
+        aria-label="link para enviar um email"
+        href="mailto:eumathfreitas@gmail.com"
+        target="_blank"
+      >
         <MdOutlineEmail />
       </Link>
       <Link
+        aria-label="link para o linkedin"
         href="https://www.linkedin.com/in/matheus-freitas-629557232/"
         target="_blank"
       >
