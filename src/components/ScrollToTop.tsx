38e58e693f0838f897e967d0879f04d15b725cc4
Matheus <mf635097@gmail.com>
2025-03-27 12:58:11 -0300

    chore: add animation to scroll to top button

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -28,9 +28,12 @@ export const ScrollToTop = () => {
       href="#"
       className={`${
         visible ? 'block' : 'hidden'
-      } fixed right-4 bottom-4 p-2 border bg-background-color rounded-full text-2xl animate-fadeIn duration-200 ease-in-out hover:bg-white hover:text-background-color hover:border-background-color`}
+      } fixed right-4 bottom-4 w-11 h-11 group border bg-background-color rounded-full text-2xl animate-fadeIn duration-200 ease-in-out hover:text-background-color hover:border-background-color`}
     >
-      <BsArrowUp />
+      <div className="relative overflow-hidden w-full h-full rounded-full flex justify-center items-center">
+        <BsArrowUp className="z-[1] relative" />
+        <div className="absolute w-full h-full inset-0 bg-white -left-8 top-8 rounded-full duration-200 ease-in-out group-hover:left-0 group-hover:top-0"></div>
+      </div>
     </a>
   );
 };
